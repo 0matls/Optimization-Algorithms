a1ee5adb55695b1e2abcd4c775b3072bb6714bdb
0matls <88787230+0matls@users.noreply.github.com>
2022-01-06 20:20:18 +0200

    Create goldenSectionMethod.m

---
+++ assignment1/goldenSectionMethod.m
@@ -0,0 +1,33 @@
+%%  Optimization Techniques
+%   1st assignement 18-11-21 goldenSectionMethod 
+%  
+
+function [a, b, k] = goldenSectionMethod(l, a0, b0, f)
+a = a0;  
+b = b0;   
+k = 0;       
+gamma = 0.618033988749;
+x1 = a0 + (1-gamma)*(b0-a0);
+x2 = a0 + gamma*(b0-a0);
+x1i = f(x1);
+x2i = f(x2);
+while (b(k+1) - a(k+1)) > l 
+    k = k + 1;
+
+    if (x1i > x2i)
+        a(k+1) = x1;
+        b(k+1) = b(k);
+        x1 = x2;
+        x2 = a(k+1) + gamma*(b(k+1)-a(k+1));
+        x1i = x2i;
+        x2i = f(x2);
+    elseif (x1i <= x2i)
+        b(k+1) = x2;
+        a(k+1) = a(k);
+        x2 = x1;
+        x1 = a(k+1) + (1-gamma)*(b(k+1)-a(k+1));
+        x2i = x1i;
+        x1i = f(x1);
+    end
+end
+end
